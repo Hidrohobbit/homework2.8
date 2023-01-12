212edd05c42066d3e79942d9b7c5840d01fe97ef
Stas Mishin <hidrohobbit@yandex.ru>
2023-01-12 20:59:21 +0400

    initial commit

---
+++ homework_2.8.1/homework_2.8.1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+int function(std::string str, int forbidden_length)
+{
+    if (forbidden_length == str.length())
+    {
+        throw std::exception("bad_length");
+    }
+    return str.length();
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+ 
+    int forbidden_length = 0;
+    bool end_while = true;
+    std::string str;
+
+    std::cout << "Введите запретную длину: ";
+    std::cin >> forbidden_length;
+    
+    while(end_while)
+    {
+        std::cout << "Введите слово: ";
+        std::cin >> str;
+        try
+        {
+            std::cout << "Длинна слова " << str << " равна " << function(str, forbidden_length) << std::endl;
+        }
+        catch (const std::exception& ex)
+        {
+            std::cout << "Вы ввели слово запретной длинны! До свидания.";
+            end_while = false;
+        }
+    }
+    return 0;
+}
